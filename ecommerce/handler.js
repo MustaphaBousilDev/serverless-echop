be09952d3daecc8db36a89b5a86e57e182e87a33
MustaphaBousilDev <bousilmustapha@gmail.com>
2024-12-15 16:09:11 +0100

    finish function delete and getAll

--- ecommerce/handler.js
+++ ecommerce/handler.js
@@ -14,3 +14,17 @@ module.exports.updateProduct = async (event) => {
     body: JSON.stringify("update  Product with this id:" + productId)
   };
 };
+
+module.exports.deleteProduct = async (event) => {
+  let productId = event.pathParameters.id;
+  return {
+    statusCode: 200,
+    body: JSON.stringify("delete  Product with this id:" + productId)
+  };
+};
+module.exports.getAllProducts = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("get All Products")
+  };
+};
